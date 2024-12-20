4cad76e3ddf48fa5064afb8eabeef3232383d377
sohyun <sohyun32253@kaywon.ac.kr>
2024-12-20 17:21:24 +0900

    '12/20'

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import vuetify from 'vite-plugin-vuetify'
 export default defineConfig({
 	assetsInclude: ['**/*.PNG', '**/*.png'],  // PNG 파일을 Vite가 올바르게 처리할 수 있도록 포함
 	logLevel: 'info',
-	 base: '/saltrain/',
+	 base: '/',
 
   plugins: [
 		vue(),
@@ -19,6 +19,12 @@ export default defineConfig({
 	resolve: {
 		alias: {
 		  '@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	  },
+	  build: {
+		outDir: 'dist', // 빌드된 파일들이 dist 폴더에 저장됩니다.
+		rollupOptions: {
+		  input: '/src/main.js' // main.js를 빌드의 시작점으로 설정
 		}
 	  }
 })
